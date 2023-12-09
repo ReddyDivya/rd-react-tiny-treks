ee9214959f1f415e534fa51adbd790fa18579da7
Divya <divyareddy0205@gmail.com>
2023-12-08 17:54:26 -0800

    react tiny projects

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (<div>
+        <h1>React Tiny Trek</h1>
+        <i>It comprises small React tasks. Click any button to observe its functionality.</i>
+    </div>)
+
+}
+
+export default Home;
